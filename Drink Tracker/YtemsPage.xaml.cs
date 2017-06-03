1e39fe41cb921f36363cfbd67fcf5d08e10d2f85
Ahimtar <misoromanek@gmail.com>
2017-06-03 14:01:28 +0200

    Account and bill names displayed in headers

--- Drink Tracker/YtemsPage.xaml.cs
+++ Drink Tracker/YtemsPage.xaml.cs
@@ -27,6 +27,26 @@ namespace Drink_Tracker
             this.InitializeComponent();
         }
 
+        Bill currentbill;
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            currentbill = (Bill)e.Parameter;
+
+            using (var db = new AccountContext())
+            {
+                var items = db.Items
+                    .Where(item => item.BillId == currentbill.BillId)
+                    .ToList();
+
+                YtemsList.ItemsSource = items;
+            }
+
+            YtemsHeaderTitle.Text = "Items on bill " + currentbill.Name;
+
+            base.OnNavigatedTo(e);
+        }
+
         private void YtemsList_ItemClick(object sender, ItemClickEventArgs e)
         {
             this.Frame.Navigate(typeof(DrinkTypesPage), e.ClickedItem);
