771f712342be746eff4958eafbf5b00d9f378368
rtrembecky <richardt@centrum.sk>
2017-06-06 10:11:34 +0200

    more converters, yay!

---
+++ Drink Tracker/Converters/ItemToName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Drink_Tracker.Converters
+{
+    public class ItemToName : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var item = value as Item;
+            return item.Drink.Name;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
