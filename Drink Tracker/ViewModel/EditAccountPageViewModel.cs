2fb6704cf9367ebbed4d9ad88767646c25955bc4
rtrembecky <richardt@centrum.sk>
2017-06-25 13:55:02 +0200

    removed some unnecessary usings

--- Drink Tracker/ViewModel/EditAccountPageViewModel.cs
+++ Drink Tracker/ViewModel/EditAccountPageViewModel.cs
@@ -1,9 +1,4 @@
 ﻿using Drink_Tracker.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Drink_Tracker.ViewModel
 {
